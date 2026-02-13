a183f4f01f4c2c54c25c94de9860bd6390105362
fraaays <202380114@psu.palawan.edu.ph>
2026-02-13 14:50:00 +0800

    modify allowed host

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin  # Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
 
@@ -8,41 +8,37 @@ class CollegeAdmin(admin.ModelAdmin):
     search_fields = ("college_name",)
     list_filter = ("created_at",)
 
-
-
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-   
-    list_display = ("program_name", "college")
-    
-    search_fields = ("program_name", "college__college_name")
-   
+    list_display = ("prog_name", "college")
+    search_fields = ("college_name", "prog_name",)
     list_filter = ("college",)
-
-
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    
     list_display = ("name", "college", "description")
-   
-    search_fields = ("name", "description")
-    
+    search_fields = ("org_name", "description",)
     list_filter = ("college",)
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    search_fields = ("lastname", "firstname")
+    search_fields = ("lastname", "firstname",)
 
 
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined")
-    search_fields = ("student__lastname", "student__firstname")
-
-    @admin.display(description='Program')
+    list_display = (
+        "student",
+        "get_member_program",
+        "organization",
+        "date_joined",
+    )
+    search_fields = ("student__lastname", "student__firstname",)
     def get_member_program(self, obj):
-       
-        return obj.student.program if obj.student else None
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
