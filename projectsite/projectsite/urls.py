c1dff8da1ae8b80e8fbb8b71e425ebff10455834
asi0723 <202380159@psu.palawan.edu.ph>
2026-02-20 17:46:52 +0800

    deleteview added

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from studentorg import views
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
-
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
@@ -32,5 +31,6 @@ urlpatterns = [
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
 
