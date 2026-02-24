89d21afbdf1d7072aa9f3268e072ae2b147c23e1
fraaays <202380114@psu.palawan.edu.ph>
2026-02-24 20:07:36 +0800

    05 Query Set, Context Data, Ordering

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -16,12 +16,31 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["organization_count"] = Organization.objects.count()
         context["program_count"] = Program.objects.count()
         return context
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
+    
+
     
 class OrganizationList(ListView):
     model = Organization
